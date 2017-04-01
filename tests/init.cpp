b9f9c0c71d6fe5d79f53bbc3b89b091655903b41
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-01 23:03:20 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,76 +1,44 @@
-#include <matrix.hpp>
+#include <BinaryTree.hpp>
 #include <catch.hpp>
 
-SCENARIO("matrix init", "[init]") 
+SCENARIO("default constructor") 
 {
-	Matrix matrix;
-	REQUIRE(matrix.line_() == 0);
-	REQUIRE(matrix.column_() == 0);
+	Tree tree;
+	REQUIRE(tree.x_() == 0);
+	REQUIRE(tree.left_() == NULL);
+	REQUIRE(tree.right_() == NULL);
 }
 
-SCENARIO("matrix with params", "[init]")
+SCENARIO("constructor with params")
 {
-	Matrix matrix(2,3);
-	REQUIRE(matrix.line_() == 2);
-	REQUIRE(matrix.column_() == 3);
+	
+}
+SCENARIO("copy constructor")
+{
+	
+}
+
+SCENARIO("add")
+{
+	
+}
+
+SCENARIO("search")
+{
+	
 }
 				
-SCENARIO("copy", "[init]")
+SCENARIO("fIn")
 {
-	Matrix matrix(2,3);
-	Matrix copy(matrix);
-	REQUIRE(copy.line_() == 2);
-	REQUIRE(copy.column_() == 3);
-}
-				
-SCENARIO("+", "[init]")
-{
-	Matrix A(2, 2);
-	Matrix B(2, 2);
-	Matrix C(2, 2);
-	std::ifstream ("A.txt") >> A;
-	std::ifstream ("B.txt") >> B;
-	std::ifstream ("A+B.txt") >> C;
-	REQUIRE((A + B) == C);
+	
 }
 
-SCENARIO("*", "[init]")
+SCENARIO("fOut")
 {
-	Matrix A(2, 2);
-	Matrix B(2, 2);
-	Matrix D(2, 2);
-	std::ifstream ("A.txt") >> A;
-	std::ifstream ("B.txt") >> B;
-	std::ifstream ("AB.txt") >> D;
-	REQUIRE((A * B) == D);
-}
-				
-SCENARIO("scan", "[init]")
-{
-	Matrix A(2, 2);
-	Matrix B(A);
-	std::ifstream("A.txt") >> A;
-	B.scan("A.txt");
-	REQUIRE(A == B);
+	
 }
 
-SCENARIO("==", "[init]")
+SCENARIO("Out")
 {
-	Matrix A(2, 2);
-	Matrix B(2, 2);
-	bool f;
-	std::ifstream("A.txt") >> A;
-	std::ifstream("A.txt") >> B;
-	if (A == B)
-		f = true;
-	REQUIRE(f == true);
-}
-
-SCENARIO("=", "[init]")
-{
-	Matrix A(2, 2);
-	Matrix B;
-	std::ifstream("A.txt") >> A;
-	B = A;
-	REQUIRE(A == B);
+	
 }

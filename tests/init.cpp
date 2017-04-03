bba3e74f7570e395a69b47dc4639a917d792a310
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-03 20:58:06 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,43 +5,32 @@ SCENARIO("default constructor")
 {
 	Tree node;
 	REQUIRE(node.x_() == 0);
-	REQUIRE(node.left_() == NULL);
-	REQUIRE(node.right_() == NULL);
 }
 
 SCENARIO("add")
 {
-	Tree tree1, tree2;
-	Node* newEl;
-	tree1.fIn("Tree.txt");
-	std::ifstream("newEl.txt") >> newEl->x;
-	tree1.add(newEl);
-	tree2.fIn("Tree+newEl.txt");
-	REQUIRE(tree1 == tree2);
+	Tree tree;
+	int newEl;
+	tree.fIn("Tree.txt");
+	std::ifstream("newEl.txt") >> newEl;
+	tree.add(newEl);
+	REQUIRE(right_() == 7);
 }
 
 SCENARIO("search")
 {
 	Tree tree;
 	Node* node;
-	tree.fIn("Tree.txt");
-	node.fIn("Node.txt");
-	REQUIRE(tree.search(7) == node);
+	tree.fIn("Tree+newEl.txt");
+	node = tree.search(5);
+	REQUIRE(x_() == 5);
+	REQUIRE(left_() == 3);
+	REQUIRE(right_() == 7);
 }
 				
 SCENARIO("fIn")
 {
 	Tree tree;
 	tree.fIn("Tree.txt");
-	REQUIRE(tree->x_ == 5);
-}
-
-SCENARIO("fOut")
-{
-	Tree tree1, tree2;
-	tree1.fIn("Tree.txt");
-	tree2.fIn("TreeOut.txt");
-	tree1.fOut("TreeOut.txt");
-	tree1.fIn("TreeOut.txt");
-	REQUIRE(tree1 == tree2);
+	REQUIRE(x_() == 5);
 }

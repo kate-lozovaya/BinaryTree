81f0e7c8e2ca56a3cebba68fd0f56ae48e274cb8
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-04 17:00:09 +0300

    Update BinaryTree.hpp

--- include/BinaryTree.hpp
+++ include/BinaryTree.hpp
@@ -1,3 +1,7 @@
+// бд.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
 #include <iostream> 
 #include <string> 
 #include <fstream> 
@@ -9,7 +13,7 @@ struct Node
 	T x;
 	Node<T> * left;
 	Node<T> * right;
-	Node(T const& value) : x{value}, left{nullptr}, right{nullptr} {}
+	Node(T const& value) : x{ value }, left{ nullptr }, right{ nullptr } {}
 };
 
 template<typename T>
@@ -20,7 +24,7 @@ private:
 public:
 	Tree()
 	{
-		root = nullptr;
+		root  = nullptr;
 	}
 
 	~Tree()
@@ -30,7 +34,7 @@ public:
 
 	void deleteNode(Node<T> * node)
 	{
-		if (node == NULL) return;
+		if (node == nullptr) return;
 		deleteNode(node->left);
 		deleteNode(node->right);
 		delete node;
@@ -51,41 +55,35 @@ public:
 		return root->right;
 	}
 
-	void add(const T newEl)
+	Node<T> * root_() const
 	{
-		if (root == nullptr)
-			root = new Node(newEl);
-		else
-		{
-			Node<T> * El = new Node<T>;
-			Node<T> *& curEl = new Node<T>;
-			curEl = root;
-			while (curEl != nullptr)
-			{
-				El = curEl;
-				if (newEl > curEl->x)
-					curEl = curEl->right;
-				else
-				if (newEl < curEl->x)
-					curEl = curEl->left;
-				else return;
+		return root;
+	}
+
+	void insert(const T& value)
+	{
+		insert(root, value);
+	}
+
+	void insert(Node<T>* & node, const T& value)
+	{
+		if (node) {
+			if (value < node->x) {
+				insert(node->left, value);
 			}
-			if (newEl > El->x)
-			{
-				El->right = new Node<T>;
-				El->right->x = newEl;
-				El->right->left = El->right->right = nullptr;
+			else if (value > node->x) {
+				insert(node->right, value);
 			}
-			else
-			{
-				El->left = new Node<T>;
-				El->left->x = newEl;
-				El->left->left = El->left->right = nullptr;
+			else {
+				return;
 			}
+		}
+		else {
+			node = new Node<T>(value);
 		}
 	}
 
-	Node<T> * search(const T& x)
+	Node<T> * search(const T& x)const
 	{
 		Node<T> * curEl = root;
 		while (curEl != nullptr)
@@ -102,18 +100,85 @@ public:
 		return curEl;
 	}
 
-	void fIn(string filename)
+	void fIn(const string filename)
 	{
 		ifstream fin;
+		unsigned int k;
 		fin.open(filename);
 		if (!fin.is_open())
 			cout << "The file isn't find" << endl;
 		else
 		{
 			deleteNode(root);
-			root = new Node<T>;
-			root->x = 0;
-			root->left = root->right = nullptr;
 			if (fin.eof()) return;
 			else
 			{
+				fin >> k;
+				T newEl;
+				for (unsigned int i = 0; i < k; ++i)
+				{
+					fin >> newEl;
+					insert(newEl);
+				}
+			}
+			fin.close();
+		}
+	}
+
+	unsigned int size(Node<T> * node)const
+	{
+		unsigned int size_ = 0;
+		if (node != nullptr)
+			size_ = size(node->left) + 1 + size(node->right);
+		return size_;
+	}
+
+	void out_to_file(const string filename)const
+	{
+		ofstream fout;
+		fout.open(filename);
+		if (!fout.is_open())
+			cout << "The file isn't find" << endl;
+		else
+		{
+			unsigned int size_ = size(root);
+			if (size_ == 0)
+				return;
+			fout << size_ << "\t";
+			for (unsigned int i = 0; i < size_; ++i)
+			{
+				fOut(root, fout);
+			}
+			fout.close();
+		}
+	}
+
+	void fOut(Node<T> * node, ostream&stream)const
+	{
+		Node<T> * curEl = node;
+		if (curEl != nullptr)
+		{
+			fOut(curEl->left, stream);
+			stream << curEl->x << " ";
+			fOut(curEl->right, stream);
+		}
+	}
+
+	void out(ostream & stream)const
+	{
+		Out(root, stream, 0);
+	}
+
+	void Out(Node<T> * node, ostream&stream, size_t level)const
+	{
+		Node<T> * curEl = node;
+		if (curEl != nullptr)
+		{
+			Out(curEl->right, stream, level + 1);
+			for (unsigned int i = 0; i < level; ++i)
+				stream << '-';
+			stream << curEl->x << endl;
+			Out(curEl->left, stream, level + 1);
+		}
+	}
+};
